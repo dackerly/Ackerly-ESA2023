c901f516fb235899239a296982136b50e7fc27ad
dackerly <dackerly@berkeley.edu>
2023-07-18 08:07:05 -0700

    Matt's simulation script, and other updates

--- scripts/quercus.R
+++ scripts/quercus.R
@@ -12,7 +12,7 @@ p50[which.min(p50$Stem.P50),]
 names(cwd)
 nct <- c(5,7,8,9,12:17)
 
-
+# -15 removes Q. pacifica outlier
 for (i in 1:length(nct)) {
   mvar <- nct[i]
   cval <- cor(cwd[-15,mvar],p50$Stem.P50[-15],use='pair')
@@ -20,3 +20,15 @@ for (i in 1:length(nct)) {
   plot(cwd[,mvar],p50$Stem.P50,pch=19,col='blue',ylim=ymn,main=paste(names(cwd)[mvar],round(cval,3)),xlab='cwd niche')
        #points(cwd[,mvar],p50$Leaf.P50,pch=19,col='red')
 }
+
+# check how my clim niches compare to Rob's
+for (i in 1:length(nct)) {
+  mvar <- nct[i]
+  cval <- cor(cwd[,mvar],p50$CWD_rs,use='pair')
+  print(cval)
+  plot(cwd[,mvar],p50$CWD_rs,pch=19,col='blue',main=paste(names(cwd)[mvar],round(cval,3)),xlab='cwd niche')
+  #points(cwd[,mvar],p50$Leaf.P50,pch=19,col='red')
+}
+
+cor(p50$CWD_rs,p50$Stem.P50,use='pair')
+cor(p50$CWD_rs[-15],p50$Stem.P50[-15],use='pair')
